c5d249abecae8ef9bd8dfd750c09511543b8d4b2
Maya <60471986+mayacodes@users.noreply.github.com>
2020-03-03 11:22:52 -0500

    Add files via upload

--- 3- Data wrangling 8.25.19.R
+++ 3- Data wrangling 8.25.19.R
@@ -1,29 +1,36 @@
+#---------------------------------------------------------
+#         CODE 3: DATA WRANGLING
+#---------------------------------------------------------
 # title: "Data wrangling"
 # author: "Maya Luetke"
-# date: "July 3, 2019"
-# last update: "July 3, 2019"
+# date: "August 25, 2019"
+# last update: "August 25, 2019"
+#---------------------------------------------------------
 
 
-#---------------------------------
-#         set directory
-#---------------------------------
+
+#---------------------------------------------------------
+#         Set directory
+#---------------------------------------------------------
 getwd()
 setwd("C:/Users/luetke/Downloads")
-#---------------------------------
+#---------------------------------------------------------
 
 
-#---------------------------------
+
+#---------------------------------------------------------
 #         Import data
-#---------------------------------
+#---------------------------------------------------------
 pop <- read.csv('pop.csv')
 ld <- read.csv('lyme.csv')
 prism <- read.csv('climate.csv')
-#---------------------------------
+#---------------------------------------------------------
 
 
-#---------------------------------
+
+#---------------------------------------------------------
 #         Load packages
-#---------------------------------
+#---------------------------------------------------------
 #install.packages("tidyverse")
 library(tidyverse)
 #install.packages("magrittr")
@@ -42,22 +49,23 @@ library(maptools)
 library(maps)
 #install.packages("plotly")
 library(plotly)
-#---------------------------------
-?maps
+#---------------------------------------------------------
 
 
-#---------------------------------
+
+#---------------------------------------------------------
 #         Look at data
-#---------------------------------
+#---------------------------------------------------------
 names(pop)
 head(pop)
 print(pop)
-#---------------------------------
+#---------------------------------------------------------
 
 
-#---------------------------------
+
+#---------------------------------------------------------
 #         Tidy data
-#---------------------------------
+#---------------------------------------------------------
 #install.packages("dplyr")
 library(dplyr)
 #install.packages("stringr")
@@ -71,21 +79,23 @@ pop %<>% mutate(year=str_replace_all(str_year,"pop",""))
 pop %<>% mutate(year=as.integer(year))
 pop %<>% mutate(fips=str_replace_all(fips,"^0",""))
 pop %<>% mutate(fips=as.integer(fips))
-#---------------------------------
+#---------------------------------------------------------
 
 
-#---------------------------------
+
+#---------------------------------------------------------
 #         Look at data
-#---------------------------------
+#---------------------------------------------------------
 names(ld)
 head(ld)
 print(ld)
-#---------------------------------
+#---------------------------------------------------------
 
 
-#---------------------------------
+
+#---------------------------------------------------------
 #         Clean data
-#---------------------------------
+#---------------------------------------------------------
 library(dplyr)
 #ld %<>% select(STNAME, CTYNAME ,starts_with("Cases2"))
 #ld %<>% gather(starts_with("Cases2"),key="str_year", value="CTYNAME") %>% na.omit
@@ -98,12 +108,13 @@ ld %<>% gather(starts_with("Cases"),key="str_year",value="cases")
 ld %<>% mutate(year=str_replace_all(str_year,"Cases","")) 
 ld %<>% mutate(year=as.integer(year))
 ld %<>% rename(state=STNAME,county=CTYNAME)
-#---------------------------------
+#---------------------------------------------------------
 
 
-#---------------------------------
+
+#---------------------------------------------------------
 #         Pad fips with zeros
-#---------------------------------
+#---------------------------------------------------------
 fips.builder<-function(st,ct){
   if (str_length(ct)==3){
     fips<-paste(as.character(st),as.character(ct),sep="") %>% as.integer
@@ -119,12 +130,13 @@ fips.builder<-function(st,ct){
 
 ld %<>% rowwise() %>% mutate(fips=fips.builder(STCODE,CTYCODE)) # takes about 10 seconds
 ld %<>% select(-c(STCODE,CTYCODE,str_year))
-#---------------------------------
+#---------------------------------------------------------
 
 
-#---------------------------------
+
+#---------------------------------------------------------
 #         Merge datasets
-#---------------------------------
+#---------------------------------------------------------
 #mydata2 <- merge(ld,prism,by=c("fips","year"))
 #mydata3 <- merge(mydata2,pop,by=c("fips","year"))
 
@@ -135,12 +147,13 @@ ld.prism.pop<-inner_join(ld.prism,pop,by=c("fips","year"))
 #length(ld.prism$fips)
 #length(ld.prism2$fips)
 #length(mydata2$fips)
-#---------------------------------
+#---------------------------------------------------------
 
 
-#---------------------------------
+
+#---------------------------------------------------------
 #         Getting summary info
-#---------------------------------
+#---------------------------------------------------------
 #average cases per year
 ld %>% ungroup %>% group_by(year) %>% summarise(avg_cases_year=mean(cases)) %>% 
     arrange(desc(avg_cases_year))
@@ -153,25 +166,27 @@ ld %>% ungroup %>% group_by(state) %>% summarise(avg_cases_state=mean(cases)) %>
 #total cases per year
 cases_by_year <- ld %>% ungroup %>% group_by(year) %>% 
     summarize(total=sum(cases)) %>% arrange(desc(total))
-#---------------------------------
+#---------------------------------------------------------
 
 
-#---------------------------------
+
+#---------------------------------------------------------
 #         Save Rda data file
-#---------------------------------
+#---------------------------------------------------------
 write_csv(ld.prism.pop,"LdPrismPop.csv")
 save(ld.prism.pop,file="get_LdPrismPop.Rda")
-#---------------------------------
+#---------------------------------------------------------
 
 
-#---------------------------------
+
+#---------------------------------------------------------
 #         Mapping
-#---------------------------------
+#---------------------------------------------------------
 #get map data for US counties and states
 county_map <- map_data("county")
 state_map <- map_data("state")
 
-## ANNOTATE FROM HERE
+#setting up map
 ag.fips <- group_by(ld.prism.pop,fips) 
 ld.16y<-summarize(ag.fips,all.cases=sum(cases))
 ld.16y<-left_join(select(ld.prism.pop,c(state,county,fips)),ld.16y)
@@ -187,7 +202,7 @@ map.ld.16y<-left_join(county_map,ld.16y)
 ggplot(map.ld.16y)+geom_point(aes(long,lat,color=log10cases),size=0.1) + 
   scale_colour_gradientn(colours=rev(rainbow(10)))
 
-## ANNOTATE FROM HERE
+#setting up map
 county_map <- map_data("county")
 state_map <- map_data("state")
 ag.fips <- group_by(ld.prism.pop,fips)
@@ -208,7 +223,6 @@ ggplot2
 
 #malke previous map interactive
 ggplotly(ggplot2)
-
 
 #make STATE map
 county_map <- map_data("county")
@@ -229,114 +243,12 @@ ggplot4 <- ggplot(map.ld.16y)+geom_polygon(aes(long,lat,group=group,fill=log10ca
   scale_fill_gradientn(colours=rev(heat.colors(10)))
 ggplot4
 
-
-
-
-#Practice map
-
+#Map figure
 p <- ggplot(data = map,
             aes(x = long, y = lat,
                 group = group, fill = region))
 
 p + geom_polygon(color = "gray90", size = 0.1) + guides(fill = FALSE)
-
 
 ggplotly(ggplot2)
-
-
-
-#---------------------------------
-#         Mapping
-#---------------------------------
-#get map data for US counties and states
-county_map <- map_data("county")
-state_map <- map_data("state")
-
-## ANNOTATE FROM HERE
-ag.fips <- group_by(ld.prism.pop,fips) 
-ld.16y<-summarize(ag.fips,all.cases=sum(cases))
-ld.16y<-left_join(select(ld.prism.pop,c(state,county,fips)),ld.16y)
-ld.16y<-distinct(ld.16y)
-ld.16y %<>% rename(region=state,subregion=county)
-ld.16y$subregion<-str_replace_all(ld.16y$subregion," County","")
-ld.16y$region<-tolower(ld.16y$region)
-ld.16y$subregion<-tolower(ld.16y$subregion)
-ld.16y$subregion<-str_replace_all(ld.16y$subregion," parish","")
-ld.16y$subregion<-str_replace_all(ld.16y$subregion," county","")
-ld.16y %<>% mutate(log10cases=log10(1+all.cases))
-map.ld.16y<-left_join(county_map,ld.16y)
-#make map
-ggplot(map.ld.16y)+geom_point(aes(long,lat,color=log10cases),size=0.1) + 
-  scale_colour_gradientn(colours=rev(rainbow(10)))
-
-## ANNOTATE FROM HERE
-county_map <- map_data("county")
-state_map <- map_data("state")
-ag.fips <- group_by(ld.prism.pop,fips)
-ld.16y<-summarize(ag.fips,all.cases=sum(cases))
-ld.16y<-left_join(select(ld.prism.pop,c(state,county,fips)),ld.16y)
-ld.16y<-distinct(ld.16y)
-ld.16y %<>% rename(region=state,subregion=county)
-ld.16y$subregion<-str_replace_all(ld.16y$subregion," County","")
-ld.16y$region<-tolower(ld.16y$region)
-ld.16y$subregion<-tolower(ld.16y$subregion)
-ld.16y$subregion<-str_replace_all(ld.16y$subregion," parish","")
-ld.16y %<>% mutate(log10cases=log10(1+all.cases))
-map.ld.16y<-left_join(county_map,ld.16y)
-#make map
-ggplot2 <- ggplot(map.ld.16y)+geom_polygon(aes(long,lat,group=group,fill=log10cases),color="gray",lwd=0.2) +
-  scale_fill_gradientn(colours=rev(heat.colors(10)))
-ggplot2
-
-#malke previous map interactive
-ggplotly(ggplot2)
-
-
-#make STATE map
-county_map <- map_data("county")
-state_map <- map_data("state")
-ag.fips <- group_by(ld.prism.pop,fips)
-ld.16y<-summarize(ag.fips,all.cases=sum(cases))
-ld.16y<-left_join(select(ld.prism.pop,c(state,county,fips)),ld.16y)
-ld.16y<-distinct(ld.16y)
-ld.16y %<>% rename(region=state,subregion=county)
-ld.16y$subregion<-str_replace_all(ld.16y$subregion," County","")
-ld.16y$region<-tolower(ld.16y$region)
-ld.16y$subregion<-tolower(ld.16y$subregion)
-ld.16y$subregion<-str_replace_all(ld.16y$subregion," parish","")
-ld.16y %<>% mutate(log10cases=log10(1+all.cases))
-state.map.ld.16y<-left_join(state_map,ld.16y)
-
-map.ld.16y$avg_cases_state <- map.ld.16y %>% group_by(region) %>% summarise(avg_cases_state=sum(log10cases))
-
-state.map.ld.16y$avg_cases_state <- state.map.ld.16y %>% group_by(region) %>% summarise(mean(log10cases))
-
-ggplot4 <- ggplot(map.ld.16y)+geom_polygon(aes(long,lat,group=group,fill=log10cases,color="gray",lwd=0.2) +
-  scale_fill_gradientn(colours=rev(heat.colors(10)))
-ggplot4
-
-
-#Practice map
-county_map <- map_data("county")
-state_map <- map_data("state")
-map <-left_join(state_map,county_map)
-p <- ggplot(data = map,
-            aes(x = long, y = lat,
-                group = group, fill = region))
-
-p + geom_polygon(color = "gray90", size = 0.1) + guides(fill = FALSE)
-
-
-#Practice map 2
-county_map <- map_data("county")
-state_map <- map_data("state")
-map <-left_join(county_map, state_map)
-p <- ggplot(data = map,
-            aes(x = long, y = lat,
-                group = group, fill = region))
-
-p + geom_polygon(color = "gray90", size = 0.1) + guides(fill = FALSE)
-
-
-
-  
+#---------------------------------------------------------
