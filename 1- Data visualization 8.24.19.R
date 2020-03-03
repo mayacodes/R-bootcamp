1d9724248e2a51383ca1be50017e013655b25c54
Maya <60471986+mayacodes@users.noreply.github.com>
2020-03-03 10:40:56 -0500

    Add files via upload

---
+++ 1- Data visualization 8.24.19.R
@@ -0,0 +1,337 @@
+# title: "Programming"
+# author: "Maya Luetke"
+# date: "July 3, 2019"
+# last update: "July 3, 2019"
+
+
+
+#---------------------------------
+#         set directory
+#---------------------------------
+getwd()
+setwd("C:/Users/luetke/Downloads")
+#---------------------------------
+
+
+#---------------------------------
+#         Import data
+#---------------------------------
+mers <- read.csv('cases.csv')
+#---------------------------------
+
+
+#---------------------------------
+#         Data checks
+#---------------------------------
+head(mers)
+class(mers$onset)
+
+mers$hospitalized[890] <- c('2015-02-20')
+mers <- mers[-471,]
+#---------------------------------
+
+
+#---------------------------------
+#         Convert factor to date
+#---------------------------------
+#change to date format
+#install.packages("lubridate")
+library(lubridate)
+mers$onset2 <- ymd(mers$onset)
+#---------------------------------
+
+
+#---------------------------------
+#         Recode errors
+#---------------------------------
+mers$hospitalized[mers$hospitalized=="???"] <- NA
+mers$hospitalized[mers$hospitalized=="3/5/14, 5/5/14"] <- NA
+
+which(mers$hospitalized != is.date)
+which(!is.Date(mers$hospitalized))
+#---------------------------------
+
+#---------------------------------
+#         Convert factor to date
+#---------------------------------
+#change to date format
+library(lubridate)
+mers$hospitalized2 <- ymd(mers$hospitalized)
+class(mers$onset2)
+#---------------------------------
+
+
+#---------------------------------
+#         Create some dates
+#---------------------------------
+#find earliest date
+day0 <- min(na.omit(mers$onset2))
+
+#create epidemic date
+mers$epi.day <- as.numeric(mers$onset2 - day0)
+#---------------------------------
+
+
+
+#---------------------------------
+#         Plotting
+#---------------------------------
+#install.packages("ggplot2")
+library(ggplot2)
+ggplot(data=mers) + 
+  geom_bar(mapping=aes(x=epi.day)) +
+  labs(x='Epidemic day', y='Case count', title='Global count of MERS cases by date of symptom onset',
+       caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+#---------------------------------
+
+
+#---------------------------------
+#         Plotting
+#---------------------------------
+ggplot(data=mers) + 
+  geom_bar(mapping=aes(x=epi.day, fill=country)) +
+  labs(x='Epidemic day', y='Case count', title='Global count of MERS cases by date of symptom onset',
+       caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+
+#add position="fill"
+ggplot(data=mers) + 
+  geom_bar(mapping=aes(x=epi.day, fill=country), position="fill") +
+  labs(x='Epidemic day', y='Case count', title='Global count of MERS cases by date of symptom onset',
+       caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+
+
+#add coord_flip()
+ggplot(data=mers) + 
+  geom_bar(mapping=aes(x=epi.day, fill=country)) +
+  labs(x='Epidemic day', y='Case count', title='Global count of MERS cases by date of symptom onset',
+       caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv") + 
+    coord_flip() 
+
+
+#add coord_flip() and coord_polar()
+ggplot(data=mers) + 
+  geom_bar(mapping=aes(x=epi.day, fill=country)) +
+  labs(x='Epidemic day', y='Case count', title='Global count of MERS cases by date of symptom onset',
+       caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv") + 
+  coord_flip() + coord_polar()
+#---------------------------------
+
+
+#---------------------------------
+#         Create new variable
+#---------------------------------
+mers$infectious.period <- mers$hospitalized2-mers$onset2    # calculate "raw" infectious period
+class(mers$infectious.period)           # these data are class "difftime"
+mers$infectious.period <- as.numeric(mers$infectious.period, units = "days") # convert to days
+#---------------------------------
+
+
+#---------------------------------
+#         Univariate plot
+#---------------------------------
+#https://www.rstudio.com/wp-content/uploads/2015/03/ggplot2-cheatsheet.pdf
+ggplot(data=mers) +
+  geom_histogram(aes(x=infectious.period)) + 
+  labs(x='Infectious period', y='Frequency', title='Distribution of calculated MERS infectious period',
+       caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+#---------------------------------
+
+
+#---------------------------------
+#         Recode for the cases that occurred BEFORE hospitalization
+#---------------------------------
+mers$infectious.period2 <- ifelse(mers$infectious.period<0,0,mers$infectious.period)
+#---------------------------------
+
+
+#---------------------------------
+#         Try univariate plots again
+#---------------------------------
+ggplot(data=mers) +
+  geom_histogram(aes(x=infectious.period2)) + 
+  labs(x='Infectious period', y='Frequency',
+       title='Distribution of calculated MERS infectious period (positive values only)', caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+#---------------------------------
+
+
+#---------------------------------
+#         Explore frequency of hospital acquired infections
+#---------------------------------
+ggplot(data=mers) + 
+  geom_density(mapping=aes(x=infectious.period2)) + 
+  labs(x='Infectious period', y='Frequency',
+       title='Probability density for MERS infectious period (positive values only)', caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+#---------------------------------
+
+
+#---------------------------------
+#         Explore frequency of hospital acquired infections wiht AREA under curve
+#---------------------------------
+ggplot(data=mers) + 
+  geom_area(stat='bin', mapping=aes(x=infectious.period2)) +
+  labs(x='Infectious period', y='Frequency',
+       title='Area plot for MERS infectious period (positive values only)', caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+#---------------------------------
+
+
+#---------------------------------
+#         Explore geom_dotplot and geom_bar.
+#---------------------------------
+#geom_dotplot
+ggplot(data=mers) + 
+  geom_dotplot(stat='bin', mapping=aes(x=infectious.period2)) +
+  labs(x='Infectious period', y='Frequency',
+       title='Area plot for MERS infectious period (positive values only)', caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+
+#geom_bar
+ggplot(data=mers) + 
+  geom_bar(stat='bin', mapping=aes(x=infectious.period2)) +
+  labs(x='Infectious period', y='Frequency',
+       title='Area plot for MERS infectious period (positive values only)', caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+#---------------------------------
+
+
+#---------------------------------
+#         Delete outlier for following graphs
+#---------------------------------
+#delete outlier
+mers$infectious.period2[mers$infectious.period2>150] <- NA
+#---------------------------------
+
+
+#---------------------------------
+#         Bivariate plots
+#---------------------------------
+library(ggplot2)
+ggplot(data=mers) + 
+  geom_bar(mapping=aes(x=epi.day, y=infectious.period2)) +
+  labs(x='Epidemic day', y='Infectious period', title='Global count of MERS cases by date of symptom onset',
+       caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv") 
+  
+ggplot(data=mers) + 
+  geom_point(aes(epi.day,infectious.period2)) +
+  labs(x='Time', y='Infectious period',
+       title='Plot for MERS infectious period over time', caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+
+ggplot(data=mers) + 
+  geom_point(aes(x=epi.day,y=infectious.period2)) +
+  labs(x='Time', y='Infectious period',
+       title='Plot for MERS infectious period over time', caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+
+ggplot(mers, aes(x=epi.day,y=infectious.period2)) + geom_line() +
+         labs(x='Time', y='Infectious period', title='Plot for MERS infectious period over time', caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+
+ggplot(mers, aes(x=epi.day,y=infectious.period2)) + geom_line() +
+  labs(x='Time', y='Infectious period', title='Plot for MERS infectious period over time', caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+
+
+# Add the regression line
+ggplot(mers, aes(x=epi.day,y=infectious.period2)) + 
+  geom_point()+
+  geom_smooth(method=lm)
+
+# Remove the confidence interval
+ggplot(mers, aes(x=epi.day,y=infectious.period2)) + 
+  geom_point()+
+  geom_smooth(method=lm, se=FALSE)
+
+# Loess method
+ggplot(mers, aes(x=epi.day,y=infectious.period2)) + 
+  geom_point()+
+  geom_smooth()
+#---------------------------------
+
+# Change point shapes, colors and sizes
+ggplot(mers, aes(x=epi.day,y=infectious.period2, color=country, scale_y_continuous())) +
+  geom_point() +
+  geom_smooth(method=loess, scale_y_continuous())
+
+ggplot(mers, aes(x=epi.day,y=infectious.period2, color = country)) +
+  geom_line(size = 1) + 
+  #geom_ribbon(aes(ymin = mers$CI.lower, ymax = mers$CI.upper, fill = country)) +
+  scale_x_continuous(breaks = c(1:12,1)) + 
+  facet_grid(. ~ avg.sh.aud.by.reg.month$year)
+
+ggplot(mers, aes(x=epi.day,y=infectious.period2, color=country)) +
+  geom_point() +
+  geom_smooth(method=lm, aes(fill=country))
+#---------------------------------
+
+
+#---------------------------------
+#         Faceting     
+#---------------------------------
+ggplot(data=mers, mapping=aes(x=epi.day, y=infectious.period2)) + 
+  geom_point(mapping = aes(color=country)) +
+  facet_wrap(~ country) + 
+  scale_y_continuous(limits = c(0, 50)) +
+  labs(x='Epidemic day', y='Infectious period',
+       title='MERS infectious period (positive values only) over time', caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+#---------------------------------
+
+
+#---------------------------------
+#         Recode gender values    
+#---------------------------------
+table(mers$gender)
+mers$gender[mers$gender=="?"] <- NA
+mers$gender[mers$gender=="M?"] <- NA
+table(mers$gender2)
+??
+#---------------------------------
+
+
+#---------------------------------
+#         More faceting     
+#---------------------------------
+ggplot(data=subset(mers, gender %in% c('M', 'F') & country %in% c('KSA', 'Oman', 'Iran', 'Jordan', 'Qatar', 'South Korea','UAE')), mapping=aes(x=epi.day, y=infectious.period2)) + 
+  geom_point(mapping = aes(color=country)) +
+  facet_grid(gender ~ country) + 
+  scale_y_continuous(limits = c(0, 50)) +
+  labs(x='Epidemic day', y='Infectious period',
+       title='MERS infectious period by gender and country', caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+#---------------------------------
+
+
+#---------------------------------
+#         Exercise. Study variation in the case fatality rate (the fraction of cases that end in death) over time and across countries.     
+#---------------------------------
+install.packages("ggplot2-exts")
+library(ggplot2-exts)
+
+names(mers)
+
+table(mers$outcome)
+outcome
+mers$fatal <- ifelse(mers$outcome)
+#case-fatality rate
+mers$death
+merse$CFR <- mers$death/mers$death
+
+ggplot(data=subset(mers, gender %in% c('M', 'F') & country %in% c('KSA', 'Oman', 'Iran', 'Jordan', 'Qatar', 'South Korea','UAE')), mapping=aes(x=epi.day, y=infectious.period2)) + 
+  geom_point(mapping = aes(color=country)) +
+  facet_grid(gender ~ country) + 
+  scale_y_continuous(limits = c(0, 50)) +
+  labs(x='Epidemic day', y='Infectious period',
+       title='MERS infectious period by gender and country', caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+#---------------------------------
+
+
+#---------------------------------
+#         Make plots interactive using plotly (#ggsave)
+#---------------------------------
+#install.packages("plotly")
+library(plotly)
+### EPI CURVE INTERACTIVE PLOT ###
+epi.curve <- ggplot(data=mers) + 
+  geom_bar(mapping=aes(x=epi.day)) +
+  labs(x='Epidemic day', y='Case count', title='Global count of MERS cases by date of symptom onset',
+       caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
+ggplotly(epi.curve)
+
+### INFECTIOUS PERIOD INTERACTIVE PLOT ###
+infectious_plot <- ggplot(mers, aes(x=epi.day,y=infectious.period2, color=country)) +
+  geom_point() + 
+  geom_smooth(method=loess)
+ggplotly(infectious_plot)
+#---------------------------------
---
+++ 3- Data wrangling 8.25.19.R
@@ -0,0 +1,342 @@
+# title: "Data wrangling"
+# author: "Maya Luetke"
+# date: "July 3, 2019"
+# last update: "July 3, 2019"
+
+
+#---------------------------------
+#         set directory
+#---------------------------------
+getwd()
+setwd("C:/Users/luetke/Downloads")
+#---------------------------------
+
+
+#---------------------------------
+#         Import data
+#---------------------------------
+pop <- read.csv('pop.csv')
+ld <- read.csv('lyme.csv')
+prism <- read.csv('climate.csv')
+#---------------------------------
+
+
+#---------------------------------
+#         Load packages
+#---------------------------------
+#install.packages("tidyverse")
+library(tidyverse)
+#install.packages("magrittr")
+library(magrittr)
+#install.packages("dplyr")
+library(dplyr)
+#install.packages("stringr")
+library(stringr)
+#install.packages("GGally")
+library(GGally)
+#install.packages("maptools")
+library(maptools)
+#install.packages("maptools")
+library(maptools)
+#install.packages("maps")
+library(maps)
+#install.packages("plotly")
+library(plotly)
+#---------------------------------
+?maps
+
+
+#---------------------------------
+#         Look at data
+#---------------------------------
+names(pop)
+head(pop)
+print(pop)
+#---------------------------------
+
+
+#---------------------------------
+#         Tidy data
+#---------------------------------
+#install.packages("dplyr")
+library(dplyr)
+#install.packages("stringr")
+library(stringr)
+
+library(dplyr)
+pop %<>% select(fips,starts_with("pop2"))
+pop %<>% gather(starts_with("pop2"),key="str_year",value="size") %>% na.omit
+table(pop$str_year)
+pop %<>% mutate(year=str_replace_all(str_year,"pop",""))
+pop %<>% mutate(year=as.integer(year))
+pop %<>% mutate(fips=str_replace_all(fips,"^0",""))
+pop %<>% mutate(fips=as.integer(fips))
+#---------------------------------
+
+
+#---------------------------------
+#         Look at data
+#---------------------------------
+names(ld)
+head(ld)
+print(ld)
+#---------------------------------
+
+
+#---------------------------------
+#         Clean data
+#---------------------------------
+library(dplyr)
+#ld %<>% select(STNAME, CTYNAME ,starts_with("Cases2"))
+#ld %<>% gather(starts_with("Cases2"),key="str_year", value="CTYNAME") %>% na.omit
+#ld %<>% mutate(year=str_replace_all(str_year,"Cases",""))
+#ld %<>% mutate(year=as.integer(year))
+#ld %<>% mutate(fips=str_replace_all(fips,"^0",""))
+#ld %<>% mutate(fips=as.integer(fips))
+
+ld %<>% gather(starts_with("Cases"),key="str_year",value="cases") 
+ld %<>% mutate(year=str_replace_all(str_year,"Cases","")) 
+ld %<>% mutate(year=as.integer(year))
+ld %<>% rename(state=STNAME,county=CTYNAME)
+#---------------------------------
+
+
+#---------------------------------
+#         Pad fips with zeros
+#---------------------------------
+fips.builder<-function(st,ct){
+  if (str_length(ct)==3){
+    fips<-paste(as.character(st),as.character(ct),sep="") %>% as.integer
+  }
+  else if (str_length(ct)==2){
+    fips<-paste(as.character(st),"0",as.character(ct),sep="") %>% as.integer
+  }
+  else {
+    fips<-paste(as.character(st),"00",as.character(ct),sep="") %>% as.integer
+  }
+  return(fips)
+}
+
+ld %<>% rowwise() %>% mutate(fips=fips.builder(STCODE,CTYCODE)) # takes about 10 seconds
+ld %<>% select(-c(STCODE,CTYCODE,str_year))
+#---------------------------------
+
+
+#---------------------------------
+#         Merge datasets
+#---------------------------------
+#mydata2 <- merge(ld,prism,by=c("fips","year"))
+#mydata3 <- merge(mydata2,pop,by=c("fips","year"))
+
+ld.prism <- inner_join(ld,prism,by=c("fips","year"))
+ld.prism.pop<-inner_join(ld.prism,pop,by=c("fips","year"))
+#ld.prism2 <- full_join(ld,prism, by=c("fips","year"))
+
+#length(ld.prism$fips)
+#length(ld.prism2$fips)
+#length(mydata2$fips)
+#---------------------------------
+
+
+#---------------------------------
+#         Getting summary info
+#---------------------------------
+#average cases per year
+ld %>% ungroup %>% group_by(year) %>% summarise(avg_cases_year=mean(cases)) %>% 
+    arrange(desc(avg_cases_year))
+  
+  
+#average cases per state
+ld %>% ungroup %>% group_by(state) %>% summarise(avg_cases_state=mean(cases)) %>% 
+    arrange(desc(avg_cases_state))
+
+#total cases per year
+cases_by_year <- ld %>% ungroup %>% group_by(year) %>% 
+    summarize(total=sum(cases)) %>% arrange(desc(total))
+#---------------------------------
+
+
+#---------------------------------
+#         Save Rda data file
+#---------------------------------
+write_csv(ld.prism.pop,"LdPrismPop.csv")
+save(ld.prism.pop,file="get_LdPrismPop.Rda")
+#---------------------------------
+
+
+#---------------------------------
+#         Mapping
+#---------------------------------
+#get map data for US counties and states
+county_map <- map_data("county")
+state_map <- map_data("state")
+
+## ANNOTATE FROM HERE
+ag.fips <- group_by(ld.prism.pop,fips) 
+ld.16y<-summarize(ag.fips,all.cases=sum(cases))
+ld.16y<-left_join(select(ld.prism.pop,c(state,county,fips)),ld.16y)
+ld.16y<-distinct(ld.16y)
+ld.16y %<>% rename(region=state,subregion=county)
+ld.16y$subregion<-str_replace_all(ld.16y$subregion," County","")
+ld.16y$region<-tolower(ld.16y$region)
+ld.16y$subregion<-tolower(ld.16y$subregion)
+ld.16y$subregion<-str_replace_all(ld.16y$subregion," parish","")
+ld.16y %<>% mutate(log10cases=log10(1+all.cases))
+map.ld.16y<-left_join(county_map,ld.16y)
+#make map
+ggplot(map.ld.16y)+geom_point(aes(long,lat,color=log10cases),size=0.1) + 
+  scale_colour_gradientn(colours=rev(rainbow(10)))
+
+## ANNOTATE FROM HERE
+county_map <- map_data("county")
+state_map <- map_data("state")
+ag.fips <- group_by(ld.prism.pop,fips)
+ld.16y<-summarize(ag.fips,all.cases=sum(cases))
+ld.16y<-left_join(select(ld.prism.pop,c(state,county,fips)),ld.16y)
+ld.16y<-distinct(ld.16y)
+ld.16y %<>% rename(region=state,subregion=county)
+ld.16y$subregion<-str_replace_all(ld.16y$subregion," County","")
+ld.16y$region<-tolower(ld.16y$region)
+ld.16y$subregion<-tolower(ld.16y$subregion)
+ld.16y$subregion<-str_replace_all(ld.16y$subregion," parish","")
+ld.16y %<>% mutate(log10cases=log10(1+all.cases))
+map.ld.16y<-left_join(county_map,ld.16y)
+#make map
+ggplot2 <- ggplot(map.ld.16y)+geom_polygon(aes(long,lat,group=group,fill=log10cases),color="gray",lwd=0.2) +
+  scale_fill_gradientn(colours=rev(heat.colors(10)))
+ggplot2
+
+#malke previous map interactive
+ggplotly(ggplot2)
+
+
+#make STATE map
+county_map <- map_data("county")
+state_map <- map_data("state")
+ag.fips <- group_by(ld.prism.pop,fips)
+ld.16y<-summarize(ag.fips,all.cases=sum(cases))
+ld.16y<-left_join(select(ld.prism.pop,c(state,county,fips)),ld.16y)
+ld.16y<-distinct(ld.16y)
+ld.16y %<>% rename(region=state,subregion=county)
+ld.16y$subregion<-str_replace_all(ld.16y$subregion," County","")
+ld.16y$region<-tolower(ld.16y$region)
+ld.16y$subregion<-tolower(ld.16y$subregion)
+ld.16y$subregion<-str_replace_all(ld.16y$subregion," parish","")
+ld.16y %<>% mutate(log10cases=log10(1+all.cases))
+map.ld.16y<-left_join(state_map,ld.16y)
+
+ggplot4 <- ggplot(map.ld.16y)+geom_polygon(aes(long,lat,group=group,fill=log10cases),color="gray",lwd=0.2) +
+  scale_fill_gradientn(colours=rev(heat.colors(10)))
+ggplot4
+
+
+
+
+#Practice map
+
+p <- ggplot(data = map,
+            aes(x = long, y = lat,
+                group = group, fill = region))
+
+p + geom_polygon(color = "gray90", size = 0.1) + guides(fill = FALSE)
+
+
+ggplotly(ggplot2)
+
+
+
+#---------------------------------
+#         Mapping
+#---------------------------------
+#get map data for US counties and states
+county_map <- map_data("county")
+state_map <- map_data("state")
+
+## ANNOTATE FROM HERE
+ag.fips <- group_by(ld.prism.pop,fips) 
+ld.16y<-summarize(ag.fips,all.cases=sum(cases))
+ld.16y<-left_join(select(ld.prism.pop,c(state,county,fips)),ld.16y)
+ld.16y<-distinct(ld.16y)
+ld.16y %<>% rename(region=state,subregion=county)
+ld.16y$subregion<-str_replace_all(ld.16y$subregion," County","")
+ld.16y$region<-tolower(ld.16y$region)
+ld.16y$subregion<-tolower(ld.16y$subregion)
+ld.16y$subregion<-str_replace_all(ld.16y$subregion," parish","")
+ld.16y$subregion<-str_replace_all(ld.16y$subregion," county","")
+ld.16y %<>% mutate(log10cases=log10(1+all.cases))
+map.ld.16y<-left_join(county_map,ld.16y)
+#make map
+ggplot(map.ld.16y)+geom_point(aes(long,lat,color=log10cases),size=0.1) + 
+  scale_colour_gradientn(colours=rev(rainbow(10)))
+
+## ANNOTATE FROM HERE
+county_map <- map_data("county")
+state_map <- map_data("state")
+ag.fips <- group_by(ld.prism.pop,fips)
+ld.16y<-summarize(ag.fips,all.cases=sum(cases))
+ld.16y<-left_join(select(ld.prism.pop,c(state,county,fips)),ld.16y)
+ld.16y<-distinct(ld.16y)
+ld.16y %<>% rename(region=state,subregion=county)
+ld.16y$subregion<-str_replace_all(ld.16y$subregion," County","")
+ld.16y$region<-tolower(ld.16y$region)
+ld.16y$subregion<-tolower(ld.16y$subregion)
+ld.16y$subregion<-str_replace_all(ld.16y$subregion," parish","")
+ld.16y %<>% mutate(log10cases=log10(1+all.cases))
+map.ld.16y<-left_join(county_map,ld.16y)
+#make map
+ggplot2 <- ggplot(map.ld.16y)+geom_polygon(aes(long,lat,group=group,fill=log10cases),color="gray",lwd=0.2) +
+  scale_fill_gradientn(colours=rev(heat.colors(10)))
+ggplot2
+
+#malke previous map interactive
+ggplotly(ggplot2)
+
+
+#make STATE map
+county_map <- map_data("county")
+state_map <- map_data("state")
+ag.fips <- group_by(ld.prism.pop,fips)
+ld.16y<-summarize(ag.fips,all.cases=sum(cases))
+ld.16y<-left_join(select(ld.prism.pop,c(state,county,fips)),ld.16y)
+ld.16y<-distinct(ld.16y)
+ld.16y %<>% rename(region=state,subregion=county)
+ld.16y$subregion<-str_replace_all(ld.16y$subregion," County","")
+ld.16y$region<-tolower(ld.16y$region)
+ld.16y$subregion<-tolower(ld.16y$subregion)
+ld.16y$subregion<-str_replace_all(ld.16y$subregion," parish","")
+ld.16y %<>% mutate(log10cases=log10(1+all.cases))
+state.map.ld.16y<-left_join(state_map,ld.16y)
+
+map.ld.16y$avg_cases_state <- map.ld.16y %>% group_by(region) %>% summarise(avg_cases_state=sum(log10cases))
+
+state.map.ld.16y$avg_cases_state <- state.map.ld.16y %>% group_by(region) %>% summarise(mean(log10cases))
+
+ggplot4 <- ggplot(map.ld.16y)+geom_polygon(aes(long,lat,group=group,fill=log10cases,color="gray",lwd=0.2) +
+  scale_fill_gradientn(colours=rev(heat.colors(10)))
+ggplot4
+
+
+#Practice map
+county_map <- map_data("county")
+state_map <- map_data("state")
+map <-left_join(state_map,county_map)
+p <- ggplot(data = map,
+            aes(x = long, y = lat,
+                group = group, fill = region))
+
+p + geom_polygon(color = "gray90", size = 0.1) + guides(fill = FALSE)
+
+
+#Practice map 2
+county_map <- map_data("county")
+state_map <- map_data("state")
+map <-left_join(county_map, state_map)
+p <- ggplot(data = map,
+            aes(x = long, y = lat,
+                group = group, fill = region))
+
+p + geom_polygon(color = "gray90", size = 0.1) + guides(fill = FALSE)
+
+
+
+  
